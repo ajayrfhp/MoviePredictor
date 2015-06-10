abbc20c5397fad41996b7bf619119bb3a6a30f0c
ajayrfhp <ajayrfhp1710@gmail.com>
2015-06-10 12:44:08 +0530

    better

--- glb.py
+++ glb.py
@@ -1,8 +1,9 @@
 ###VARIALBES
 from sklearn import linear_model
 from sklearn import svm
+from key import *
 MOVIENAME='Mad Max Fury Road'
-APIKEY='5b248f61321474b5f3e42b34ac06d315'
+
 departments=['Sound','Camera','Costume & Make-Up','Crew','Directing','Writing','Visual Effects','Production','Art','Editing']
 genres=['Action','Adventure','Animation','Comedy','Crime','Documentary','Drama','Family','Fantasy','Foreign','History','Horror','Music','Mystery','Romance','Science Fiction','TV movie','Thriller','War','Western']
 productionCompaniesPopluate=[]
